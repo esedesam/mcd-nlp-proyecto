b6925bdeb8a35258c3c082ec16416e23c691300a
samuel <ortegasamuel777@gmail.com>
2024-04-26 10:04:05 +0200

    Añadida capa numerica. Mejorado control de capas

--- visualizeMap.py
+++ visualizeMap.py
@@ -1,11 +1,15 @@
 import folium
 import json
+import folium.plugins
 import numpy as np
 import pandas as pd
+from branca.colormap import linear
 
-# Load GeoJSON data (https://valencia.opendatasoft.com/explore/dataset/barris-barrios/export/)
+# Load Barrios GeoJSON (https://valencia.opendatasoft.com/explore/dataset/barris-barrios/export/)
 with open('data/barris-barrios.geojson') as f:
     geoData = json.load(f)
+
+# Load Distritos GeoJSON (https://valencia.opendatasoft.com/explore/dataset/districtes-distritos/export/)
 
 # Load Color Mapping
 with open('data/colorMappingBarrios.json') as f:
@@ -32,11 +36,23 @@ for feature in geoData['features']:
     else:
         feature['properties'][varName] = None
         raise UserWarning(f"No data found for {feature['properties']['nombre']}.")
+    
+# Create Linear Colormap Based on Numeric Variable
+numVarName = 'dummyValue'
+colormap = linear.YlGn_09.scale(
+    dataDf[numVarName].min(), dataDf[numVarName].max())
+colormapDict = dataDf.set_index(barrioColName)[numVarName]
 
 # Create a base map
-myMap = folium.Map(location = [39.475324399975754, -0.3525380336296802], zoom_start = 11)
+myMap = folium.Map(
+    location = [39.475324399975754, -0.3525380336296802],
+    zoom_start = 11,
+    control_scale = True)
 
 # Add Barrios de Valencia to the map
+groupBarrios = folium.FeatureGroup('Barrios de Valencia').add_to(myMap)
+popupField = ['nombre']
+popupField.extend(dataVarName.tolist())
 folium.GeoJson(
     geoData,
     style_function = lambda feature: {
@@ -45,9 +61,32 @@ folium.GeoJson(
         'weight': 1,
         'fillOpacity': 0.5
     },
+    zoom_on_click = True,
     tooltip = folium.GeoJsonTooltip(fields = ['nombre'], labels = False),
-    popup = folium.GeoJsonPopup(fields = dataVarName.tolist(), localize = False)
+    popup = folium.GeoJsonPopup(fields = popupField, localize = False)
+).add_to(groupBarrios)
+
+# Add Color-Codified Numeric Variable
+groupNumVar = folium.FeatureGroup(numVarName).add_to(myMap)
+folium.GeoJson(
+    geoData,
+    style_function = lambda feature: {
+        'fillColor': colormap(colormapDict[feature['properties']['nombre']]),
+        'color': 'black',
+        'weight': 1,
+        'dashArray': '5, 5',
+        'fillOpacity': 0.5,
+    },
+    tooltip = folium.GeoJsonTooltip(fields = ['nombre'], labels = False),
+    popup = folium.GeoJsonPopup(fields = [numVarName], localize = False)
+).add_to(groupNumVar)
+
+# Add Layer Control
+# folium.LayerControl().add_to(myMap)
+folium.plugins.GroupedLayerControl(
+    groups = {'Capas': [groupBarrios, groupNumVar]},
+    collapsed = True,
 ).add_to(myMap)
 
 # Save the map to an HTML file
-myMap.save("barris.html")
+myMap.save('barris.html')
